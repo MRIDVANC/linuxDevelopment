4c62f4b8115833df484514a730d4e42b43e899df
mridvanc <24781973+MRIDVANC@users.noreply.github.com>
2023-10-29 01:16:24 +0300

    System monitoring

--- hardware/monitorSystem.sh
+++ hardware/monitorSystem.sh
@@ -1,42 +1,61 @@
 #!/bin/bash
+# Bu satır betiğin başlangıcını belirtir ve hangi kabuk (shell) dilini kullanacağınızı tanımlar. Genellikle /bin/bash kullanılır.
 
-# Determine the network interface names
+# Kablosuz ve kablolu ağ arayüz isimlerini belirle
 wired_interface="eth0"
 wireless_interface="wlan0"
 
-# Function to get network data in MB
+# MB cinsinden ağ verilerini almak için bir fonksiyon tanımla
 get_network_data() {
     local interface="$1"
+    # İstenen ağ arayüzünün gelen (rx) ve giden (tx) bayt miktarlarını oku
     local rx_bytes
     rx_bytes=$(cat "/sys/class/net/$interface/statistics/rx_bytes")
     local tx_bytes
     tx_bytes=$(cat "/sys/class/net/$interface/statistics/tx_bytes")
+    # Bu bayt miktarlarını MB cinsine dönüştür
     local network_sent_mb
-    network_sent_mb=$(awk "BEGIN {printf \"%.2f\", $rx_bytes / 1024 / 1024}")
+    network_sent_mb=$(awk "BEGIN {printf \"%.2f\", $tx_bytes / 1024 / 1024}")
     local network_recv_mb
-    network_recv_mb=$(awk "BEGIN {printf \"%.2f\", $tx_bytes / 1024 / 1024}")
+    network_recv_mb=$(awk "BEGIN {printf \"%.2f\", $rx_bytes / 1024 / 1024}")
+    # MB cinsinden gelen ve giden veriyi döndür
     echo "$network_recv_mb $network_sent_mb"
 }
 
-# Check if the wired interface exists, and if it does, use it for network info
-if [[ -e "/sys/class/net/$wired_interface" ]]; then
-    # shellcheck disable=SC2207
-    network_data=($(get_network_data "$wired_interface"))
-else
-    # If wired interface doesn't exist, use the wireless interface
-    network_data=($(get_network_data "$wireless_interface"))
-fi
+# Belirtilen ağ arayüzü için ağ verilerini al
+get_network_data_for_interface() {
+    local interface="$1"
+    # Eğer belirtilen ağ arayüzü mevcutsa ağ verilerini al, yoksa bir hata mesajı gönder
+    if [[ -e "/sys/class/net/$interface" ]]; then
+        get_network_data "$interface"
+    else
+        echo "Arayüz $interface bulunamadı"
+    fi
+}
 
-# Get CPU and memory data
+# CPU ve bellek verilerini al
 cpu_percent=$(top -n 1 | awk '/%Cpu/{print $2}')
+cpu_cores=$(nproc)
 memory_total=$(free -h | awk '/Mem/{print $2}')
 memory_used=$(free -h | awk '/Mem/{print $3}')
 
-# Display the information
+# Bilgileri ekrana yazdır
 echo "====================="
 echo "CPU Kullanımı: $cpu_percent%"
+echo "Toplam CPU Çekirdek Sayısı: $cpu_cores"
 echo "Toplam Bellek: $memory_total"
 echo "Kullanılan Bellek: $memory_used"
-echo "Ağ Gönderilen: ${network_data[0]} MB"
-echo "Ağ Alınan: ${network_data[1]} MB"
+echo "====================="
+
+# Kablolu arayüz için ağ verilerini göster
+echo "Ağ Verileri - Kablolu (Wired) Arayüz:"
+network_data=($(get_network_data_for_interface "$wired_interface"))
+echo "Toplam İndirilen: ${network_data[0]} MB"
+echo "Toplam Yüklenen: ${network_data[1]} MB"
+
+# Kablosuz arayüz için ağ verilerini göster
+echo "Ağ Verileri - Kablosuz (Wireless) Arayüz:"
+network_data=($(get_network_data_for_interface "$wireless_interface"))
+echo "Toplam İndirilen: ${network_data[0]} MB"
+echo "Toplam Yüklenen: ${network_data[1]} MB"
 echo "====================="
